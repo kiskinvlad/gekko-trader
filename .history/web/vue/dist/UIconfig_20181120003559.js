1832ff2cbfefdd7f1c4bbd449d59980809d80131
Vlad <vladyslav.kiskin@applicature.com>
2018-11-20 00:48:16 +0200

    change conf

--- .history/web/vue/dist/UIconfig_20181120003559.js
+++ .history/web/vue/dist/UIconfig_20181120003559.js
@@ -23,8 +23,8 @@
 const CONFIG = {
   headless: true,
   api: {
-      host: '0.0.0.0',
-      port: process.env.PORT || 3000,
+      host: '127.0.0.1',
+      port: 3000,
   },
   ui: {
       ssl: true,
